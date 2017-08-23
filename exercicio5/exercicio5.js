49b0c76e1a21ee619096aeb689c5307e00487444
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-08-22 22:00:53 -0300

    atualizado exercicio 5

--- exercicio5/exercicio5.js
+++ exercicio5/exercicio5.js
@@ -1,27 +1,25 @@
-/*
- * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
- * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
- */
+let num = 23279250;
+let divide = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-numSmallest = 2018;
+function resto(teste){ 
+    return teste > 0;
+};
 
-while (true) {
+// function menorNum(number){
+//     let calcula;
+//     for(let i=1;i <=20; i++){
+//        calcula map((el)=> number % el);
+//     }
+// }
 
-    numSmallest = numSmallest + 1;
-
-    teste(numSmallest);
-
-    if (result == 0) {
-        console.log(numSmallest);
-        return false;
+function menorNum(number) {
+    let calcula = divide.map((el)=> number % el);
+    if (calcula.find(resto)){
+        number++;
+        menorNum(number);
+    } else{
+        return console.log(number);
     }
 }
 
-function teste(num) {
-
-    myArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    myArray.forEach(function (element) {
-        return num % element;
-    });
-}
+menorNum(num);
