a114b6e297fe4e7efd68151943fdfbd6a5987d43
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-09-19 13:29:21 -0300

    completed exercise 6

---
+++ exercicio6/exercicio6.js
@@ -0,0 +1,49 @@
+/* 
+
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+*/
+
+let naturalNumber = 10;
+let naturalSquare = 10;
+let sumNumber = 0;
+let numberSum = 0;
+let myArray = [];
+
+
+function squareSum() {
+
+    for (var i = 0; i <= naturalNumber; i++) {
+
+        sumNumber += i;
+        console.log(sumNumber)
+    }
+    return Math.pow(sumNumber, 2);
+}
+
+function sumSquare() {
+
+    for (var i = 0; i <= naturalSquare; i++) {
+        
+        numberSum =+ i;
+        myArray.push(Math.pow(numberSum, 2));
+    }
+
+    var total = myArray.reduce(function(a, b) {
+        return a + b;
+    });
+    return total;
+}
+
+
+
+console.log(squareSum());
+console.log(sumSquare());
