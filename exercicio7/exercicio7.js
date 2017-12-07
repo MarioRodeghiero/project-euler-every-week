dd68bc4e27974aa15bad93d050ba0788124b0c96
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-12-06 22:06:02 -0200

    edited ex07

--- exercicio7/exercicio7.js
+++ exercicio7/exercicio7.js
@@ -3,30 +3,3 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 */
-
-let primo = 2;
-let i = 2;
-
-while ( i <= 20) {
-    if ( primo % i === 0) {
-        console.log(`${primo}`);
-        primo ++;
-        i++;
-    }
-    console.log(`${primo}`);
-    primo ++;
-    i++;
-}
-
-
-
-
-
-// for(let i = 0; i < 14; i++) {
-//     if (primo % 2 == 0) {
-//         primo++;
-//     }
-//     if (primo % i == 0) {
-//         console.log(`${primo}`);
-//         primo++;
-//     }
